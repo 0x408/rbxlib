8701b8dc155ca0005a3f5cc38555250dd172e7d1
0x408 <83191185+0x408@users.noreply.github.com>
2023-08-04 19:31:02 -0400

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -186,7 +186,7 @@ local themes = { -- ignore
     };
 }
 local themeobjects = {};
-local library = {priorities = {}, friends = {}, notiflist = {ntifs = {}, interval = 12}, settings = {folder_name = "jithack/"..startUpArgs[1];default_accent = Color3.fromRGB(255,255,255)}, drawings = {}, theme = table.clone(themes.Default),currentcolor = nil, flags = {}, open = false, mousestate = services.InputService.MouseIconEnabled, holder = nil, connections = {}, notifications = {}, gradient = nil};
+local library = {priorities = {}, friends = {}, notiflist = {ntifs = {}, interval = 12}, settings = {folder_name = "jithack/"..startUpArgs[1];default_accent = Color3.fromRGB(255,255,255)}, drawings = {}, theme = table.clone(themes.Default),currentcolor = nil, flags = {}, open = false, mousestate = services.InputService.MouseIconEnabled, cursor = nil, holder = nil, connections = {}, notifications = {}, gradient = nil};
 local decode = (syn and syn.crypt.base64.decode) or (crypt and crypt.base64decode) or base64_decode;
 local flags = {};
 local configignores = {};
@@ -361,21 +361,21 @@ library:instance('ImageButton', {
     Transparency = 1;
 })
 
---local cursor1 = library:create('Quad', { Filled = true, Theme = 'Accent', ZIndex = 1500 });
---local cursor2 = library:create('Quad', { Filled = true, Color = Color3.new(), ZIndex = 1499 });
+local cursor1 = library:create('Quad', { Filled = true, Theme = 'Accent', ZIndex = 1500 });
+local cursor2 = library:create('Quad', { Filled = true, Color = Color3.new(), ZIndex = 1499 });
 
 library:connect(game:GetService('RunService').RenderStepped, function()
- --   if cursor1.Visible then
-  --      local pos = game:GetService('UserInputService'):GetMouseLocation();
-   --     cursor1.PointA = pos + Vector2.new(0, 3);
-   --     cursor1.PointB = pos;
-  --      cursor1.PointC = pos + Vector2.new(3, 0);
-   --     cursor1.PointD = pos + Vector2.new(3, 3);
+    if cursor1.Visible then
+        local pos = game:GetService('UserInputService'):GetMouseLocation();
+        cursor1.PointA = pos + Vector2.new(0, 3);
+        cursor1.PointB = pos;
+        cursor1.PointC = pos + Vector2.new(3, 0);
+        cursor1.PointD = pos + Vector2.new(3, 3);
         --
- --       cursor2.PointA = cursor1.PointA + Vector2.new(1, 1)
-    --    cursor2.PointB = cursor1.PointB + Vector2.new(1, 1)
-  --      cursor2.PointC = cursor1.PointC + Vector2.new(1, 1)
-  --      cursor2.PointD = cursor1.PointD + Vector2.new(1, 1)
+        cursor2.PointA = cursor1.PointA + Vector2.new(1, 1)
+        cursor2.PointB = cursor1.PointB + Vector2.new(1, 1)
+        cursor2.PointC = cursor1.PointC + Vector2.new(1, 1)
+        cursor2.PointD = cursor1.PointD + Vector2.new(1, 1)
     end
 end)
 
@@ -399,8 +399,8 @@ function library:set_open(bool)
         self.open = bool;
         screenGui.Enabled = bool
         self.holder.Visible = bool
-  --      cursor1.Visible = bool
-   --     cursor2.Visible = bool
+        cursor1.Visible = bool
+        cursor2.Visible = bool
         local ContextActionService = game:GetService("ContextActionService")
         local InputService = game:GetService('UserInputService')
         local originalState = InputService.MouseIconEnabled
