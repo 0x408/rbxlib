d81c6c25750c70c04f0d00a68ced823b21e38b1e
0x408 <83191185+0x408@users.noreply.github.com>
2023-08-05 04:00:44 -0400

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -424,7 +424,7 @@ function library:set_open(bool)
             );
             InputService.MouseIconEnabled = false
         else
-            InputService.MouseIconEnabled = true
+            InputService.MouseIconEnabled = originalState
             ContextActionService:UnbindAction("Scrolling");
             ContextActionService:UnbindAction("Input");
         end;
